9ae299b34d752230adf4de731036a9db9341b109
Paulov <pargsoft@hotmail.com>
2023-11-29 10:09:26 +0000

    Ensure QuickReloadMag animation still works on Clients

--- Source/Coop/Player/FirearmControllerPatches/FirearmController_QuickReloadMag_Patch.cs
+++ Source/Coop/Player/FirearmControllerPatches/FirearmController_QuickReloadMag_Patch.cs
@@ -1,117 +1,83 @@
-﻿//using Newtonsoft.Json;
-//using SIT.Coop.Core.Web;
-//using SIT.Core.Coop.ItemControllerPatches;
-//using SIT.Tarkov.Core;
-//using StayInTarkov;
-//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Reflection;
-//using UnityEngine;
+﻿using EFT;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using Comfort.Common;
+using BepInEx.Logging;
+using StayInTarkov;
+using StayInTarkov.Coop;
+using StayInTarkov.Coop.Web;
 
-//namespace SIT.Core.Coop.Player.FirearmControllerPatches
-//{
-//    public class FirearmController_QuickReloadMag_Patch : ModuleReplicationPatch
-//    {
-//        public override Type InstanceType => typeof(EFT.Player.FirearmController);
-//        public override string MethodName => "QuickReloadMag";
+namespace SIT.Core.Coop.Player.FirearmControllerPatches
+{
+    public class FirearmController_QuickReloadMag_Patch : ModuleReplicationPatch
+    {
+        public override Type InstanceType => typeof(EFT.Player.FirearmController);
+        public override string MethodName => "QuickReloadMag";
 
-//        protected override MethodBase GetTargetMethod()
-//        {
-//            var method = ReflectionHelpers.GetMethodForType(InstanceType, MethodName);
-//            return method;
-//        }
+        private static HashSet<string> _Processed = new();
 
-//        [PatchPostfix]
-//        public static void Postfix(
-//            EFT.Player.FirearmController __instance
-//            , EFT.Player ____player
-//            , MagazineClass magazine)
-//        {
-//            Dictionary<string, object> magAddressDict = new();
-//            ItemAddressHelpers.ConvertItemAddressToDescriptor(magazine.CurrentAddress, ref magAddressDict);
+        protected override MethodBase GetTargetMethod()
+        {
+            var method = ReflectionHelpers.GetMethodForType(InstanceType, MethodName);
+            return method;
+        }
 
-//            Dictionary<string, object> dictionary = new()
-//            {
-//                { "fa.id", __instance.Item.Id },
-//                { "fa.tpl", __instance.Item.TemplateId },
-//                { "mg.id", magazine.Id },
-//                { "mg.tpl", magazine.TemplateId },
-//                { "ma", magAddressDict },
-//                { "m", "ReloadMag" }
-//            };
-//            AkiBackendCommunicationCoop.PostLocalPlayerData(____player, dictionary);
+        private ManualLogSource GetLogger()
+        {
+            return GetLogger(typeof(FirearmController_QuickReloadMag_Patch));
+        }
 
-//            // Quick Reload is not Round Tripped. 
-//            HasProcessed(typeof(FirearmController_QuickReloadMag_Patch), ____player, dictionary);
-//        }
+        public override void Enable()
+        {
+            _Processed.Clear();
+            base.Enable();
+        }
 
-//        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
-//        {
-//            if (HasProcessed(GetType(), player, dict))
-//                return;
+        [PatchPostfix]
+        public static void PostPatch(EFT.Player ____player)
+        {
+            var player = ____player;
+            if (player == null)
+            {
+                Logger.LogError("Unable to obtain Player variable from Firearm Controller!");
+                return;
+            }
 
-//            Logger.LogInfo("FirearmController_QuickReloadMag_Patch:Replicated");
-
-//            if (player.HandsController is EFT.Player.FirearmController firearmCont)
-//            {
-//                try
-//                {
-
-//                    var ma = JsonConvert.DeserializeObject<Dictionary<string, object>>(dict["ma"].ToString());
-//                    ItemAddressHelpers.ConvertDictionaryToAddress(ma, out var magAddressGrid, out var magAddressSlot);
-
-//                    var magazine = player.Profile.Inventory.GetAllItemByTemplate(dict["mg.tpl"].ToString())
-//                        .FirstOrDefault(x => x.Id == dict["mg.id"].ToString()) as MagazineClass;
-//                    if (magazine == null)
-//                    {
-//                        Logger.LogError("FirearmController_QuickReloadMag_Patch:Replicated:Unable to find Magazine!");
-//                        return;
-//                    }
-
-//                    //firearmCont.QuickReloadMag(magazine, (IResult) =>
-//                    //{
+            Dictionary<string, object> dictionary = new()
+            {
+                { "m", "QuickReloadMag" }
+            };
+            AkiBackendCommunicationCoop.PostLocalPlayerData(player, dictionary, true, out _, out var generatedData);
+            _Processed.Add(generatedData.SITToJson());
+        }
 
 
 
-//                    //});
-//                    firearmCont.StartCoroutine(QuickReloadMag(player, firearmCont, magazine));
+        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
+        {
+            GetLogger(typeof(FirearmController_QuickReloadMag_Patch)).LogDebug("Replicated");
+
+            if (_Processed.Contains(dict.SITToJson()))
+                return;
+
+            _Processed.Add(dict.SITToJson());
+
+            if (player.HandsController is EFT.Player.FirearmController firearmCont)
+            {
+                try
+                {
+                    firearmCont.FirearmsAnimator.ReloadFast(true);
+                }
+                catch (Exception e)
+                {
+                    GetLogger().LogError(e);
+                }
+            }
+        }
 
 
-//                }
-//                catch (Exception e)
-//                {
-//                    Logger.LogError(e);
-//                }
-//            }
-//        }
-
-//        private IEnumerator QuickReloadMag(EFT.Player player
-//            , EFT.Player.FirearmController firearmCont
-//            , MagazineClass magazine)
-//        {
-//            while (!firearmCont.CanStartReload())
-//            {
-//                yield return new WaitForSeconds(1);
-//                yield return new WaitForEndOfFrame();
-//            }
-
-//            GetLogger(typeof(FirearmController_QuickReloadMag_Patch)).LogDebug($"{player.ProfileId} Notify to not use ICH Move Patch");
-//            ItemControllerHandler_Move_Patch.DisableForPlayer.Add(player.ProfileId);
-
-//            firearmCont.QuickReloadMag(magazine, (c) =>
-//            {
-
-//                if (c.Failed)
-//                {
-//                    GetLogger(typeof(FirearmController_QuickReloadMag_Patch)).LogError($"{player.ProfileId}: Failed to QuickReloadMag");
-//                }
-
-//            });
-//            GetLogger(typeof(FirearmController_QuickReloadMag_Patch)).LogDebug($"{player.ProfileId} Notify to use ICH Move Patch");
-//            ItemControllerHandler_Move_Patch.DisableForPlayer.Remove(player.ProfileId);
-
-//        }
-//    }
-//}
+    }
+}
